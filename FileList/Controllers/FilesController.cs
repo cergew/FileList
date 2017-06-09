36b9158334f52bc96678a88b7fe13f1d00ff4ab7
Павлов Сергей Германович <cergew@gmail.com>
2017-06-09 14:00:32 +0400

    Added Files controller with some default views. Changed default route to Files controler.

---
+++ FileList/Controllers/FilesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FileList.Models;
+
+namespace FileList.Controllers
+{
+    public class FilesController : Controller
+    {
+        private FilelistEntities db = new FilelistEntities();
+
+        // GET: Files
+        public ActionResult Index()
+        {
+            return View(db.Files.ToList());
+        }
+
+        // GET: Files/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            File file = db.Files.Find(id);
+            if (file == null)
+            {
+                return HttpNotFound();
+            }
+            return View(file);
+        }
+
+        // GET: Files/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Files/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Filename,Filesize,Timestamp")] File file)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Files.Add(file);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(file);
+        }
+
+        // GET: Files/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            File file = db.Files.Find(id);
+            if (file == null)
+            {
+                return HttpNotFound();
+            }
+            return View(file);
+        }
+
+        // POST: Files/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Filename,Filesize,Timestamp")] File file)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(file).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(file);
+        }
+
+        // GET: Files/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            File file = db.Files.Find(id);
+            if (file == null)
+            {
+                return HttpNotFound();
+            }
+            return View(file);
+        }
+
+        // POST: Files/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            File file = db.Files.Find(id);
+            db.Files.Remove(file);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
